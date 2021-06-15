9f4c314908dfbbc112e6cd9500bc5b1faf51e47b
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-06-15 16:21:31 -0400

    add: url endpoints

---
+++ tradovate_api/utils/urls.py
@@ -0,0 +1,20 @@
+##-----------------------------##
+## Tradovate PyAPI             ##
+## Written By: Ryan Smith      ##
+##-----------------------------##
+## URLs                        ##
+##-----------------------------##
+
+## Imports
+
+## Constants
+# -Base URLs
+base_live = "https://live.tradovateapi.com/v1/"
+base_demo = "https://demo.tradovateapi.com/v1/"
+# -Authorization
+auth_me = "auth/me/"
+# -Account URLs
+account_list = "account/list/"
+
+
+## Classes
