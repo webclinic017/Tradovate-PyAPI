9a1c24731e7474da6eb9f1ed0c2741ffb1d8bc84
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-06 02:39:18 -0400

    add: Profile class

---
+++ tradovate_api/profile/__init__.py
@@ -0,0 +1,18 @@
+##-------------------------------##
+## Tradovate PyAPI               ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## Tradovate Profile Class       ##
+##-------------------------------##
+
+## Imports
+from __future__ import annotations
+
+
+## Classes
+class Profile:
+    """Tradovate Profile"""
+
+    # -Constructor
+    def __init__(self) -> Profile:
+        pass
